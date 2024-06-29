5ca97c4dfb5f4fcf6b6696433815c4fdc7c20961
Eureka <fengyuan-liang@foxmail.com>
2024-06-29 20:26:04 +0800

    update Dockerfile.admin

--- apps/mxclub-admin/main.go
+++ apps/mxclub-admin/main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"github.com/fengyuan-liang/jet-web-fasthttp/jet"
+	"github.com/fengyuan-liang/jet-web-fasthttp/pkg/xlog"
+	"mxclub/apps/mxclub-admin/config"
+	_ "mxclub/apps/mxclub-admin/controller"
+)
 
+func main() {
+	xlog.SetOutputLevel(xlog.Ldebug)
+	jet.AddMiddleware(jet.TraceJetMiddleware, jet.RecoverJetMiddleware)
+	jet.Run(config.GetConfig().Server.Port)
 }
