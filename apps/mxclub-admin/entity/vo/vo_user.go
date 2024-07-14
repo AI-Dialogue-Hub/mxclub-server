d06aa75ae009beb4681ace548ef3a57faf7bc01a
Eureka <fengyuan-liang@foxmail.com>
2024-07-14 21:24:24 +0800

    修改编号问题

--- apps/mxclub-admin/entity/vo/vo_user.go
+++ apps/mxclub-admin/entity/vo/vo_user.go
@@ -20,6 +20,6 @@ type UserVO struct {
 	WxGrade      string        `json:"wx_grade"`                                     // 微信等级
 	Role         enum.RoleType `json:"role" validate:"required" reg_err_info:"不能为空"` // 用户权限
 	DisPlayName  string        `json:"disPlayName"`                                  // 用户权限
-	MemberNumber string        `json:"member_number"`                                // 编号
+	MemberNumber int           `json:"member_number"`                                // 编号
 	ActivatedAt  time.Time     `json:"activated_at"`                                 // 最近一次上线
 }
