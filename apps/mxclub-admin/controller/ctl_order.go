6f24e962a63d12503548a9c108238de829bffbb8
Eureka <75926969+fengyuan-liang@users.noreply.github.com>
2024-08-05 15:54:25 +0800

    Update ctl_order.go

--- apps/mxclub-admin/controller/ctl_order.go
+++ apps/mxclub-admin/controller/ctl_order.go
@@ -30,7 +30,7 @@ func (ctl OrderController) PostV1OrderList(ctx jet.Ctx, req *req.OrderListReq) (
 	return xjet.WrapperResult(ctx, pageResult, err)
 }
 
-func (ctl OrderController) DeleteV1Order(ctx jet.Ctx, param *api.PathParam) (*api.Response, error) {
+func (ctl OrderController) DeleteV1Order0(ctx jet.Ctx, param *api.PathParam) (*api.Response, error) {
 	orderId, _ := param.GetInt64(0)
 	err := ctl.OrderService.OrderRepo.RemoveByID(orderId)
 	return xjet.WrapperResult(ctx, "ok", err)
