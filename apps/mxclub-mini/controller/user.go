749b77adb9902805320e6f9cc41d463c9fb12f99
Eureka <fengyuan-liang@foxmail.com>
2025-02-05 09:50:07 +0800

    feat: 暂时屏蔽注销打手功能

--- apps/mxclub-mini/controller/user.go
+++ apps/mxclub-mini/controller/user.go
@@ -169,5 +169,6 @@ func (ctl UserController) GetV1AssistantStatus(ctx jet.Ctx) (*api.Response, erro
 }
 
 func (ctl UserController) PostV1AssistantRemove(ctx jet.Ctx) (*api.Response, error) {
-	return xjet.WrapperResult(ctx, "注销成功，请重新登录", ctl.userService.RemoveAssistant(ctx))
+	return nil, errors.New("请联系客服进行注销")
+	//return xjet.WrapperResult(ctx, "注销成功，请重新登录", ctl.userService.RemoveAssistant(ctx))
 }
