1d6e0181189f6fc3dff1ae4da9c49a992d6106e5
Eureka <fengyuan-liang@foxmail.com>
2025-08-13 01:01:42 +0800

    feat: 添加是否假支付页面配置

--- domain/common/entity/enum/mini_config.go
+++ domain/common/entity/enum/mini_config.go
@@ -17,6 +17,7 @@ var (
 	SalesThreshold      MiniConfigEnum = "sales_threshold"        // 出销量热门标阈值
 	PayOrderWarningInfo MiniConfigEnum = "pay_order_warning_info" // 订单页面warning信息
 	OrderReward         MiniConfigEnum = "order_reward"           // 打赏金额
+	payOrderFaker       MiniConfigEnum = "pay_order_faker"        // 打赏金额
 )
 
 var miniConfigEnumMap = map[MiniConfigEnum]string{
@@ -34,6 +35,7 @@ var miniConfigEnumMap = map[MiniConfigEnum]string{
 	SalesThreshold:      "出销量热门标阈值",
 	PayOrderWarningInfo: "订单页面warning信息",
 	OrderReward:         "打赏金额(直接输入数字不要带单位)",
+	payOrderFaker:       "假下单页面(1表示开启，其他为关闭)",
 }
 
 func (m MiniConfigEnum) IsValid() bool {
