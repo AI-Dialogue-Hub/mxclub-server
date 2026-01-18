b39bc13722fe534aa0607e40c44f1ee5c01924c5
Eureka <fengyuan-liang@foxmail.com>
2026-01-18 15:19:05 +0800

    feat: 消费金额折扣修改

--- domain/order/entity/enum/enum_discount.go
+++ domain/order/entity/enum/enum_discount.go
@@ -4,12 +4,12 @@ var DiscountRules = map[string]struct {
 	Threshold float64
 	Discount  float64
 }{
-	"LV1": {Threshold: 500, Discount: 0.98},
-	"LV2": {Threshold: 2000, Discount: 0.95},
-	"LV3": {Threshold: 5000, Discount: 0.92},
-	"LV4": {Threshold: 10000, Discount: 0.90},
-	"LV5": {Threshold: 20000, Discount: 0.85},
-	"LV6": {Threshold: 50000, Discount: 0.80},
+	"LV1": {Threshold: 500, Discount: 0.99},
+	"LV2": {Threshold: 2000, Discount: 0.97},
+	"LV3": {Threshold: 5000, Discount: 0.95},
+	"LV4": {Threshold: 10000, Discount: 0.92},
+	"LV5": {Threshold: 20000, Discount: 0.89},
+	"LV6": {Threshold: 50000, Discount: 0.85},
 }
 
 func FetchDiscountByGrade(grade string) float64 {
